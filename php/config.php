3e5993621bcb0949bb1f4350599f7f93b5b9151f
Nil Nguyen <nilnguyenkl@gmail.com>
2021-05-20 18:46:49 +0700

    Account have ajax

--- php/config.php
+++ php/config.php
@@ -1,10 +1,11 @@
 <?php
-  $hostname = "localhost:3306";
+  $hostname = "localhost";
   $username = "root";
-  $password = "";
+  $password = "nilnguyen123";
   $dbname = "chatapp";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  
   if(!$conn){
     echo "Database connection error".mysqli_connect_error();
   }
